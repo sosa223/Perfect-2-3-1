3e128d52bb12cb497fade9f0cc16726deae5183f
Chief <75649719+sosa223@users.noreply.github.com>
2025-02-16 19:18:26 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("Hello World\n");
-  return 0;
+
+int input() {
+  int num;
+  char c;
+  printf("Enter a number: ");
+        if (scanf("%d", &num) == 1) {
+            return num; 
+        } 
+        // Added this for error message if user inputs character for inpur.
+        else {  
+            printf("You have a invalid input, Please input only numbers and try again.\n"); 
+            while ((c = getchar()) != '\n'); 
+            return input(); 
+        }
+    }
+
+
+int perfect(int num) {
+    if (num < 2) return 0; 
+
+    int sum = 1; 
+    for (int n = 2; n * n <= num; n++) {
+        if (num % n == 0) {
+            sum += n;
+            if (n != num / n) {
+                sum += num / n; } 
+        }
+    
+    }
+    return sum == num;
+}
+
+
+void inputResult(int num, int isPerfect) {
+    if (isPerfect)
+        printf("%d is a Perfect Number\n", num);
+    else
+        printf("%d is Not a Perfect Number\n", num);
+}
+
+int main() {
+    int number = input(); 
+    int result = perfect(number); 
+    inputResult(number, result); 
+
+    return 0;
 }
